6c449a1171d4a028365bb03836f2a756d3fdab1e
Flávio Sousa <flavio.jsousa@gmail.com>
2013-09-25 12:51:28 +0100

    pathToNGNS

---
+++ src/pathToNGNS.js
@@ -0,0 +1,31 @@
+module.exports = function pathToNGNS(path){
+
+  console.log(path);
+
+  var outputString = '';
+
+  var currentPath = path['averageCase'];
+  for (var i = 0; i < currentPath.length -1; i++)
+    outputString += currentPath[i][0] + ',' + currentPath[i][1] + ';' ;
+
+  outputString += currentPath[currentPath.length -1][0] + ',' + currentPath[currentPath.length -1][1];
+
+
+  outputString += '|';
+
+  var currentPath = path['bestCase'];
+  for (var i = 0; i < currentPath.length -1 ; i++)
+    outputString += currentPath[i][0] + ',' + currentPath[i][1] + ';' ;
+
+  outputString += currentPath[currentPath.length -1][0] + ',' + currentPath[currentPath.length -1][1];
+
+  outputString += '|';
+
+  var currentPath = path['worstCase'];
+  for (var i = 0; i < currentPath.length -1; i++)
+    outputString += currentPath[i][0] + ',' + currentPath[i][1] + ';' ;
+
+  outputString += currentPath[currentPath.length -1][0] + ',' + currentPath[currentPath.length -1][1];
+
+  return outputString;
+};
