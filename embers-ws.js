02156a6555a71d9c6cbe2b03d01654394d5239f8
Flávio Sousa <flavio.jsousa@gmail.com>
2014-01-23 16:07:12 +0000

    post reply to full stochastic sim

--- embers-ws.js
+++ embers-ws.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var printKml = require('./src/printKml');
+var demoPrintKml = require('./src/demoPrintKml');
 var join = require('path').join;
 
 var cors = require('cors')
@@ -9,6 +10,7 @@ app.use(cors());
 
 var ngnsAPI = require('ngnsAPI');
 var demoAPI = require('demoAPI');
+var fullEmbers = require('embers');
 
 app.configure(function(){
   app.use(express.bodyParser());
@@ -16,6 +18,7 @@ app.configure(function(){
 
 app.use('/outputs', express.static(__dirname + '/outputs/'));
 
+//NGNS
 app.post ('/embersNGNS', function (req, res){
 
   var id = Math.random()*100000;
@@ -40,12 +43,13 @@ app.post ('/embersNGNS', function (req, res){
   });
 });
 
+//Demo
 app.post ('/runEmbers', function (req, res){
   var id = Math.random()*100000;
   id = id.toFixed(0);
 
   console.log('\n>req ' + id + ' @', Date());
-  console.log('Running Embers...');
+  console.log('Running Embers demo...');
 
   if (!req.body)
     return res.send(409, 'No content in body');
@@ -53,18 +57,83 @@ app.post ('/runEmbers', function (req, res){
   var opts = req.body;
 
   demoAPI(opts, function(err, kml, pathArrays){
+
     if (err) {
       console.log(err);
-      res.send(err);
-    }
-
-    var outputPath = join(__dirname, 'outputs');
-    printKml(kml, id, outputPath, function (){
-      
-      res.send({reqId: id});
       console.log('Done.');
       console.log('res ' + id + ' @', Date());
-    });
+      return res.send({reqId: null, err: err});
+    } else {
+
+      var outputPath = join(__dirname, 'outputs');
+      demoPrintKml(kml, id, outputPath, function (err){
+
+        if (err) {
+          console.log(err);
+          console.log('Done.');
+          console.log('res ' + id + ' @', Date());
+          return res.send({reqId: null, err: err});
+        }
+
+        console.log('Done.');
+        console.log('res ' + id + ' @', Date());
+        return res.send({reqId: id, err: null});
+      });
+    }
+  });
+});
+
+//Full Stochastic Embers
+app.post ('/fullEmbers', function (req, res){
+  var id = Math.random()*100000;
+  id = id.toFixed(0);
+
+  console.log('\n>req ' + id + ' @', Date());
+  console.log('Running Full Stochastic Embers...');
+
+  if (!req.body) {
+    return res.send(409, 'No content in body');
+  }
+
+  var opts = req.body;
+
+  fullEmbers(opts, function(err, kml){
+
+    if (err) {
+      console.log(err);
+      console.log('Done.');
+      console.log('res ' + id + ' @', Date());
+      return res.send({reqId: null, err: err});
+    } else {
+
+      var outputPath = join(__dirname, 'outputs');
+
+      printKml(kml[mean1h], 'mean1h', id, outputPath, onKmlWrite);
+      printKml(kml[mean2h], 'mean2h', id, outputPath, onKmlWrite);
+      printKml(kml[icUp1h], 'icUp1h', id, outputPath, onKmlWrite);
+      printKml(kml[icUp2h], 'icUp2h', id, outputPath, onKmlWrite);
+      printKml(kml[icLo1h], 'icLo1h', id, outputPath, onKmlWrite);
+      printKml(kml[icLo2h], 'icLo2h', id, outputPath, onKmlWrite);
+
+      //count kml size
+      var countDown = Object.keys(kml).length;
+      function onKmlWrite(err){
+        if (--countDown > 0 )
+          continue;
+
+        if (err) {
+          console.log(err);
+          console.log('Done.');
+          console.log('res ' + id + ' @', Date());
+          return res.send({reqId: null, err: err});
+        }
+
+        console.log('Done.');
+        console.log('res ' + id + ' @', Date());
+        return res.send({reqId: id, err: null});
+      }
+
+    }
   });
 });
 
--- src/demoPrintkml.js
+++ src/demoPrintkml.js
@@ -2,26 +2,27 @@ var fs = require('fs');
 var join = require('path').join
 module.exports = printKml;
 
-
-//scenario can be: 'worstCase', 'bestCase', 'averageCase'
-function printKml (kml, scenario, id, outputPath, callback){
+function printKml (kml, id, outputPath, callback){
 
   fs.mkdir(outputPath, '777', onDirCreate);
 
   function onDirCreate(err){
 
-    writeFile();
+    writeFile('worstCase');
+    writeFile('bestCase');
+    writeFile('averageCase');
 
   }
+
   function writeFile(scenario){
     var filePath = join(outputPath, 'output_'+ id + '-' + scenario +'.kml');
-    fs.writeFile(filePath, kml, {encoding:'utf8', mode: 0644}, onFileWrite);
+    fs.writeFile(filePath, kml[scenario], {encoding:'utf8', mode: 0644}, onFileWrite);
   }
 
+  var fileCounter = 3;
   function onFileWrite(err) {
-    if (err) {
-      callback (err);
-    } else {
+    if (err) callback (err);
+    if (--fileCounter === 0) {
       callback(null);
     }
   }
